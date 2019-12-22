de20a95f926ef9033f193de62b8c8df0778126e1
Lucas Mendonça <lsm5@cin.ufpe.br>
2019-12-21 21:04:04 -0300

    chore: req appointments

--- Server/src/routes.js
+++ Server/src/routes.js
@@ -7,6 +7,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentsController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -20,6 +21,9 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentsController.store);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
