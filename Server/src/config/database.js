dade6f42167d7cf66d8c3240be02f1635ef2fd7f
Lucas Mendonça <lsm5@cin.ufpe.br>
2019-12-31 00:30:06 -0300

    feat: environment var

--- Server/src/config/database.js
+++ Server/src/config/database.js
@@ -1,9 +1,10 @@
+require('dotenv/config');
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'gobarber',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.PASS,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
