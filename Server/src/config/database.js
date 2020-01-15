434c333aca1106600093ae4f08bcffdbe2f12d55
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-01-15 16:04:08 -0300

    chore: return avatar and name provider

--- Server/src/config/database.js
+++ Server/src/config/database.js
@@ -1,9 +1,10 @@
 require('dotenv/config');
+
 module.exports = {
   dialect: 'postgres',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
-  password: process.env.PASS,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   define: {
     timestamps: true,
