0374f90fbcda6797f2d1031786672da4a737609e
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-01-17 00:38:35 -0300

    chore: using unform

--- FrontWeb/src/pages/_layouts/auth/styles.js
+++ FrontWeb/src/pages/_layouts/auth/styles.js
@@ -10,7 +10,7 @@ export const Wrapper = styled.div`
 `;
 
 export const Content = styled.div`
-  width: 100%
+  width: 100%;
   max-width: 315px;
   text-align: center;
 
@@ -19,17 +19,17 @@ export const Content = styled.div`
     flex-direction: column;
     margin-top: 30px;
 
-    input{
-      background: rgba(0,0,0,0.1);
+    input {
+      background: rgba(0, 0, 0, 0.1);
       border: 0;
       border-radius: 4px;
       height: 44px;
       padding: 0 15px;
-      color: #FFF;
+      color: #fff;
       margin: 0 0 10px;
 
-      &::placeholder{
-        color: rgba(255,255,255,0.7)
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
       }
     }
 
@@ -38,19 +38,19 @@ export const Content = styled.div`
       height: 44px;
       background: #3b9eff;
       font-weight: bold;
-      color: #FFF;
+      color: #fff;
       border: 0;
       border-radius: 4px;
       font-size: 16px;
       transition: background 0.2s;
 
       &:hover {
-        background: ${darken(0.03, '#3b9eff')}
+        background: ${darken(0.03, '#3b9eff')};
       }
     }
 
-    a{
-      color: #FFF;
+    a {
+      color: #fff;
       margin-top: 15px;
       font-size: 16px;
       opacity: 0.8;
@@ -60,5 +60,4 @@ export const Content = styled.div`
       }
     }
   }
-
 `;
