71fdd4eeaf1e3576516606045e51cd2f5c664bf8
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-01-17 00:30:59 -0300

    style: styled auth

--- FrontWeb/src/pages/SingUp/index.js
+++ FrontWeb/src/pages/SingUp/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import logo from '~/assets/logo.svg';
 // import { Container } from './styles';
 
-export default function SingUp() {
-  return <div />;
+export default function SignUp() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
