e26680ef8db2c86f9482bfd91e88d3643ba4c89a
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-01-23 18:28:53 -0300

    feat: add avatar profile

--- FrontWeb/src/pages/Profile/index.js
+++ FrontWeb/src/pages/Profile/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Form, Input } from '@rocketseat/unform';
 import { useSelector, useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
+import AvatarInput from './AvatarInput';
 import { Container } from './styles';
 
 export default function Profile() {
@@ -17,20 +18,21 @@ export default function Profile() {
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
 
         <hr />
 
         <Input
-          type="password"
           name="oldPassword"
+          type="password"
           placeholder="Sua senha atual"
         />
         <Input type="password" name="password" placeholder="Nova senha" />
         <Input
-          type="password"
           name="confirmPassword"
+          type="password"
           placeholder="Confirmação de senha"
         />
         <button type="submit">Atualizar perfil</button>
