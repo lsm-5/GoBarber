3e65dc50a3f886c205a0de783985fc46048aa63f
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-01-25 18:40:41 -0300

    fix: bug delay profile

--- FrontWeb/src/pages/Profile/AvatarInput/index.js
+++ FrontWeb/src/pages/Profile/AvatarInput/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useRef, useEffect } from 'react';
 import { useField } from '@rocketseat/unform';
 import api from '~/services/api';
@@ -20,7 +21,7 @@ export default function AvatarInput() {
         path: 'dataset.file',
       });
     }
-  }, [ref, registerField]);
+  }, [ref.current]);
 
   async function handleChange(e) {
     const data = new FormData();
@@ -38,12 +39,7 @@ export default function AvatarInput() {
   return (
     <Container>
       <label htmlFor="avatar">
-        <img
-          src={
-            preview || 'https://api.adorable.io/avatars/50/abott@adorable.png'
-          }
-          alt=""
-        />
+        <img src={preview} alt="" />
 
         <input
           type="file"
