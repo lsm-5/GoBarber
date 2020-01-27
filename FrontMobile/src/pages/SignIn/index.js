cf1ae41ec4379ddfa4cb16096d92fd4112efc02e
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-01-26 23:11:38 -0300

    feat: components globals

--- FrontMobile/src/pages/SignIn/index.js
+++ FrontMobile/src/pages/SignIn/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text } from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+
+import Background from '~/components/Background';
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <View />;
+  return (
+    <Background>
+      <Text>Lucas</Text>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+      <Button>Entrar</Button>
+    </Background>
+  );
 }
