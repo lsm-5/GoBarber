6068f3c07fec09410334ec98f2be1f955a0a9c7c
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-01-26 21:43:53 -0300

    init: start project

---
+++ FrontMobile/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+import './config/ReactotronConfig';
+
+// import { Container } from './styles';
+console.tron.log('Hello Programmer');
+
+export default function src() {
+  return <View />;
+}
