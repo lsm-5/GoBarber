9c960463731bd226ca11b974e346c16042980ab6
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-01-27 17:53:27 -0300

    config: redux

--- FrontMobile/src/index.js
+++ FrontMobile/src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import 'react-native-gesture-handler';
 
 import '~/config/ReactotronConfig';
 
+import { store, persistor } from '~/store';
 import Routes from '~/routes';
 
 // import { Container } from './styles';
 
 export default function src() {
   return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <Routes />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+        <Routes />
+      </PersistGate>
+    </Provider>
   );
 }
