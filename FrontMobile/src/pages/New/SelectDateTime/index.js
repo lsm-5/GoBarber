bdb043538522037eba4b3313b9ce27b63866376d
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-01-29 16:14:26 -0300

    feat: page SelectDateTime and Confirm

--- FrontMobile/src/pages/New/SelectDateTime/index.js
+++ FrontMobile/src/pages/New/SelectDateTime/index.js
@@ -1,32 +1,69 @@
-import React, { useState } from 'react';
-import { TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { TouchableOpacity } from 'react-native';
+
+import api from '~/services/api';
 
 import Background from '~/components/Background';
 import DateTimeInput from '~/components/DateTimeInput';
-import { Container } from './styles';
 
-export default function SelectDateTime() {
+import { Container, HourList, Hour, Title } from './styles';
+
+export default function SelectDateTime({ navigation }) {
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+
+  const provider = navigation.getParam('provider');
+
+  useEffect(() => {
+    async function loadAvailable() {
+      const response = await api.get(`providers/${provider.id}/available`, {
+        params: {
+          date: date.getTime(),
+        },
+      });
+      setHours(response.data);
+    }
+    loadAvailable();
+  }, [date, provider.id]);
+
+  function handleSelectHour(time) {
+    navigation.navigate('Confirm', {
+      provider,
+      time,
+    });
+  }
 
   return (
     <Background>
       <Container>
         <DateTimeInput date={date} onChange={setDate} />
+        <HourList
+          data={hours}
+          keyExtractor={item => item.time}
+          renderItem={({ item }) => (
+            <Hour
+              enabled={item.available}
+              onPress={() => handleSelectHour(item.value)}
+            >
+              <Title>{item.time}</Title>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   );
 }
 
 SelectDateTime.navigationOptions = ({ navigation }) => ({
-  title: 'Selecione o horário',
+  title: 'Selecione o Horário',
   headerLeft: () => (
     <TouchableOpacity
       onPress={() => {
         navigation.goBack();
       }}
     >
-      <Icon name="chevron-left" size={20} color="#FFF" />
+      <Icon name="chevron-left" size={20} color="#fff" />
     </TouchableOpacity>
   ),
 });
